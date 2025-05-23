97bd765ab8e5fd5a519bc547173eddefcfd33676
josabett <jos.sanguinetti@duocuc.cl>
2025-05-22 22:01:16 -0400

    first commit

---
+++ src/main/java/com/alfondobar/alfondobar/AlfondobarApplication.java
@@ -0,0 +1,13 @@
+package com.alfondobar.alfondobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfondobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfondobarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfondobar/alfondobar/AlfondobarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfondobar.alfondobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfondobarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
