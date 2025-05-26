59b7786245a66bb3bf635d1afa6505adef60e130
josabett <jos.sanguinetti@duocuc.cl>
2025-05-25 20:30:53 -0400

    cambios static

--- src/main/java/com/alfondobar/alfondobar/HomeController.java
+++ src/main/java/com/alfondobar/alfondobar/HomeController.java
@@ -1,12 +1,13 @@
 package com.alfondobar.alfondobar;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index() {
         return "index"; // templates/index.html
     }
